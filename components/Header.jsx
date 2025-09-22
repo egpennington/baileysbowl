44b62865ad75b3b6b49f3fcb61f2bef3c21db9be
egpennington <egpennington@hotmail.com>
2025-09-22 15:31:20 -0800

    change ref to bulldoglogo2 in public folder

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ import logo from '/src/assets/logo.png'
 export default function Header() {
     return (
         <div className="header">                      
-                <img src="/public/bulldogLogo2.png" alt="bulldog logo" />
+                <img src="/bulldogLogo2.png" alt="bulldog logo" />
                 <h1 className="tagline">turn what you have into what you crave</h1>          
         </div>
     )
