eb0977057366af4bd32127046a00f310ec749622
egpennington <egpennington@hotmail.com>
2025-09-22 20:30:52 -0800

    add h2 to recipe return and aria lable

--- components/ClaudeRecipe.jsx
+++ components/ClaudeRecipe.jsx
@@ -2,7 +2,8 @@ import ReactMarkdown from 'react-markdown'
 
 export default function ClaudeRecipe(props){
 return (
-    <section className="suggested-recipe-container">
+    <section className="suggested-recipe-container" aria-live="polite">
+        <H2>Chef Bailey Recommends:</H2>
         <ReactMarkdown>{props.recipe}</ReactMarkdown>
     </section>
 )
