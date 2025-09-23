f660b4e85c3f812e537fb248971017365cf4c6c0
egpennington <egpennington@hotmail.com>
2025-09-22 20:26:28 -0800

    add css for recipe container

--- components/ClaudeRecipe.jsx
+++ components/ClaudeRecipe.jsx
@@ -2,7 +2,7 @@ import ReactMarkdown from 'react-markdown'
 
 export default function ClaudeRecipe(props){
 return (
-    <section>
+    <section className="suggested-recipe-container">
         <ReactMarkdown>{props.recipe}</ReactMarkdown>
     </section>
 )
