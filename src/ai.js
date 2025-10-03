a1f40ab3b4a107f12a50878259ead3738e70423a
egpennington <egpennington@hotmail.com>
2025-10-03 12:12:29 -0800

    fix bugs for recipe

--- src/ai.js
+++ src/ai.js
@@ -2,21 +2,17 @@
 
 import Anthropic from "@anthropic-ai/sdk"  // npm install
 
+const SYSTEM_PROMPT = `
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
+`
+
 const anthropic = new Anthropic({    
     apiKey: import.meta.env.VITE_ANTHROPIC_API_KEY,    
     dangerouslyAllowBrowser: true,
 })
 
-function getRecipe(ingredients, recipeType) {
-  const chosen = recipeType || "general";
-}
-
-const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a ${chosen} recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
-`
-
 // async returns a promise
-export async function getRecipeFromChefClaude(ingredientsArr, recipeType = "") {
+export async function getRecipeFromChefClaude(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ")
 
     const msg = await anthropic.messages.create({
